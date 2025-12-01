ff6f44e485ebdba5ae04ee4814fd7945f489d41b
maikeldugrau <maikelkappaunn599@gmail.com>
2025-12-01 12:42:38 -0300

    Update DungeonAPIProvider.java

--- main/java/plugindungeon/api/DungeonAPIProvider.java
+++ main/java/plugindungeon/api/DungeonAPIProvider.java
@@ -1,1 +1,8 @@
+package plugindungeon.api;
+
+public class DungeonAPIProvider {
+    private static DungeonAPI api;
+    public static void setAPI(DungeonAPI instance) { api = instance; }
+    public static DungeonAPI get() { return api; }
+}
 
