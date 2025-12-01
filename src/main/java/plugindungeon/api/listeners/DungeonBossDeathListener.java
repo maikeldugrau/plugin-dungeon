e402bb0aadf39c06df5bd2cc942bcdd7b49cdf7e
maikeldugrau <maikelkappaunn599@gmail.com>
2025-12-01 15:22:31 -0300

    Update DungeonBossDeathListener.java

--- src/main/java/plugindungeon/api/listeners/DungeonBossDeathListener.java
+++ src/main/java/plugindungeon/api/listeners/DungeonBossDeathListener.java
@@ -1,8 +1,7 @@
 package plugindungeon.api.listeners;
 
 import org.bukkit.entity.Player;
-import org.bukkit.entity.LivingEntity;
 
 public interface DungeonBossDeathListener {
-    void onBossDeath(Player player, LivingEntity boss, String dungeonId, int floor);
+    void onDungeonBossDeath(String dungeonId, Player killer);
 }
