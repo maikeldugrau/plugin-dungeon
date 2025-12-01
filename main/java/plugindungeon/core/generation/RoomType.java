7015e4cf54458836c6f3e0700597c416849747c1
maikeldugrau <maikelkappaunn599@gmail.com>
2025-12-01 12:44:20 -0300

    Update RoomType.java

--- main/java/plugindungeon/core/generation/RoomType.java
+++ main/java/plugindungeon/core/generation/RoomType.java
@@ -1,1 +1,17 @@
+package plugindungeon.core.generation;
+
+public enum RoomType {
+    START,
+    STANDARD,
+    TRAP,
+    PLATFORM,
+    FIRE_CHAMBER,
+    SHADOW_HALL,
+    BLOOD_CRYPT,
+    STONE_CAVERN,
+    ARCANE_LIBRARY,
+    MINIBOSS,
+    BOSS,
+    TREASURE
+}
 
